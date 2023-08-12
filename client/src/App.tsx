74b576747fdc091431589d643448d8a7a20e6975
gwonyejun <kyjuni1059@gmail.com>
2023-08-12 15:00:04 +0900

    예시 하나 더 작성을 해보기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -36,6 +36,46 @@
 // export default App;
 
 
+// import React, { useState, useRef } from 'react';
+
+// import './App.css';
+
+// function App() {
+//   const [count, SetCount] = useState(0)
+//   const countRef = useRef(0);
+//   let countNumber = 0;
+
+//   const doRenderIng = () =>{
+//     SetCount(count +1)
+//   }
+
+//   const increaseRef = () => {
+//     countRef.current += 1; // countRef.current의 값이 + 1씩 증가 한다.
+//     console.log('countRef.current: ', countRef.current)
+//     //useRef의 값을 확인 할 수 있도록 콘소로 표시해 준다.
+//   }
+
+//   const increaseNumber = () => {
+//     countNumber += 1;
+//     console.log('countNumber: ', countNumber)
+
+//   }
+
+
+//   return (
+//     <div className="App">
+//       <p>Ref: {countRef.current}</p>
+//       <p>Number: {countNumber}</p>
+//       <button onClick={increaseRef}>increaseRef 올려</button>
+//       <button onClick={increaseNumber}>increaseNumber 올려</button>
+//       <button onClick={doRenderIng}>doRenderIng 렌더링</button>
+  
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState, useRef } from 'react';
 
 import './App.css';
